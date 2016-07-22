1116ff069c4cc214bf48c7d4d9464297384cc2a1
mreed19 <michael.reed315@gmail.com>
2016-07-22 11:02:58 -0400

    Add preflight middleware and create a POST route for creating a new post.

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,5 +1,12 @@
 module.exports = (req, res, next) => {
   // Allow CORS
   res.header('Access-Control-Allow-Origin', '*');
+
+  // Allow Content-Type header (for JSON payloads)
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Allow more HTTP verbs
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+
   next();
 }
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -8,4 +8,23 @@ router.get('/', (req, res) => {
     .then(posts => res.json(posts));
 });
 
+router.post('/', (req, res) => {
+  var post = new Post({
+    title: req.body.title,
+    link_url: req.body.link_url,
+    body: req.body.body
+  });
+
+  post
+    .save()
+    .then(
+      postData => {
+        res.json({
+          message: 'Successfully created a post',
+          post: postData
+        });
+      }
+    );
+});
+
 module.exports = router;
